81314bc0f82cebff32db9bd6da67c776ead09134
pattypatpat2632 <oleary.audio@gmail.com>
2017-10-09 11:48:47 -0400

    remove secret.Swift from gitignore

---
+++ C4QWeatherApp/Utility/Secret.swift
@@ -0,0 +1,14 @@
+//
+//  Secret.swift
+//  C4QWeatherApp
+//
+//  Created by Patrick O'Leary on 10/6/17.
+//  Copyright © 2017 Patrick O'Leary. All rights reserved.
+//
+
+import Foundation
+
+final class Client {
+    static let id = "JEedKf3ZpiUsMW9tvTHm3"
+    static let secret = "A4UNZPxGoAFm0JBbi5Vx8I0ZVXmHGGIMpcQDIKbj"
+}
